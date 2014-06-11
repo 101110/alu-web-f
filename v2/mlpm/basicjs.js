fdf2113c9099c98cd6769bad1314aa61e101477d
jonathan-barbero <jonathan.barbero@gmail.com>
2014-06-11 01:31:02 -0300

    Menú cool a medio andar

--- v2/mlpm/basicjs.js
+++ v2/mlpm/basicjs.js
@@ -1,8 +1,236 @@
+// JS Aray instead HTML Markup
+
+var arrMenu = [
+	{
+		title: 'Aluar',
+		icon: 'fa fa-reorder',
+		items: [
+			{
+				name: 'Quiénes Somos',
+				icon: 'fa fa-laptop',
+				link: '#',
+				items: [
+					{
+						title: 'Quiénes Somos',
+						icon: 'fa fa-laptop',
+						items: [
+											{
+												name: 'Aluar en Cifras',
+												link: '#'
+											},
+											{
+												name: 'Historia',
+												link: '#'
+											},
+											{
+												name: 'Estructura de la compañía',
+												link: '#'
+											},
+											{
+												name: 'Compañías del Grupo',
+												link: '#'
+											},
+											{
+												name: 'Sistemas de Calidad y Gestión Industrial',
+												link: '#'
+											}
+						]
+					}
+				]
+			},
+			{
+				name: 'El Aluminio',
+				icon: 'fa fa-phone',
+				link: '#',
+				items: [
+					{
+						title: 'El Aluminio',
+						icon: 'fa fa-phone',
+						items: [
+							{
+								name: 'Usos y Aplicaciones',
+								link: '#'
+							},
+							{
+								name: 'Historia de la Industria del Aluminio',
+								link: '#'
+							},
+							{
+								name: 'Productores',
+								link: '#'
+							},
+							{
+								name: 'Consumidores',
+								link: '#'
+							},
+							{
+								name: 'Proceso productivo',
+								link: '#'
+							}
+						]
+					}
+				]
+			},
+			{
+				name: 'Desarrollo Sustentable',
+				icon: 'fa fa-tags',
+				link: '#',
+				items: [
+					{
+						title: 'Desarrollo Sustentable',
+						icon: 'fa fa-tags',
+						items: [
+							{
+								name: 'Sistema Productivo',
+								link: '#'
+							},
+							{
+								name: 'Reportes de Sustentabilidad',
+								link: '#'
+							},
+							{
+								name: 'Salud y Seguridad',
+								link: '#'
+							},
+							{
+								name: 'Medioambiente',
+								link: '#'
+							},
+							{
+								name: 'Compromiso con la Comunidad',
+								link: '#'
+							}
+						]
+					}
+				]
+			},
+			{
+				name: 'Productos',
+				icon: 'fa fa-shopping-cart',
+				link: '#',
+				items: [
+					{
+						title: 'Productos',
+						icon: 'fa fa-shopping-cart',
+						items: [
+							{
+								name: 'División Elaborados',
+								link: '#'
+							},
+							{
+								name: 'División Primario',
+								link: '#'
+							},
+							{
+								name: 'Calidad',
+								link: '#'
+							},
+							{
+								name: 'Logística',
+								link: '#'
+							},
+							{
+								name: 'Ventas por Región',
+								link: '#'
+							},
+							{
+								name: 'Desarrollo de Nuevos Productos',
+								link: '#'
+							}
+						]
+					}
+				]
+			},
+			{
+				name: 'Información Financiera y Contable',
+				icon: 'fa fa-book',
+				link: '#',
+				items: [
+					{
+						title: 'Información Financiera y Contable',
+						icon: 'fa fa-book',
+						items: [
+							{
+								name: 'Resumen Financiero',
+								link: '#'
+							},
+							{
+								name: 'Balances y Memorias',
+								link: '#'
+							},
+							{
+								name: 'Hechos Relevantes',
+								link: '#'
+							},
+							{
+								name: 'Acciones',
+								link: '#'
+							},
+							{
+								name: 'Comisión Nacional de Valores',
+								link: '#'
+							}
+						]
+					}
+				]
+			},
+			{
+				name: 'Trabajar en Aluar',
+				icon: 'fa fa-male',
+				link: '#',
+				items: [
+					{
+						title: 'Trabajar en Aluar',
+						icon: 'fa fa-male',
+						items: [
+							{
+								name: 'Becas',
+								link: '#'
+							},
+							{
+								name: 'Programas de Pasantías',
+								icon: 'fa fa-female',
+								link: '#',
+								items: [
+									{
+										title: 'Programas de Pasantías',
+										icon: 'fa fa-female',
+										items: [
+											{
+												name: 'Programa de pasantías secundarios',
+												link: '#'
+											},
+											{
+												name: 'Programa de pasantías universitarias',
+												link: '#'
+											}
+										]
+									}
+								]
+							},
+							{
+								name: 'Envíanos tu CV',
+								link: '#'
+							}
+						]
+					}
+				]
+			},
+			{
+				name: 'Contactos',
+				link: '#'
+			}
+		]
+	}
+];
+
+
 $(document).ready(function(){
 	// JS Array implementation, overlap mode
 	$( '#menu' ).multilevelpushmenu({
 		menu: arrMenu,
 		containersToPush: [$( '#pushobj' )],
+		collapsed: true,
 
 		// Just for fun also changing the look of the menu
 		wrapperClass: 'mlpm_w',
@@ -10,265 +238,3 @@ $(document).ready(function(){
 	});
 });
 
-// JS Aray instead HTML Markup
-
-var arrMenu = [
-	{
-		title: 'All Categories',
-		icon: 'fa fa-reorder',
-		items: [
-			{
-				name: 'Devices',
-				icon: 'fa fa-laptop',
-				link: '#',
-				items: [
-					{
-						title: 'Devices',
-						icon: 'fa fa-laptop',
-						items: [
-							{
-								name: 'Mobile Phones',
-								icon: 'fa fa-phone',
-								link: '#',
-								items: [
-									{
-										title: 'Mobile Phones',
-										icon: 'fa fa-phone',
-										link: '#',
-										items: [
-											{
-												name: 'Super Smart Phone',
-												link: '#'
-											},
-											{
-												name: 'Thin Magic Mobile',
-												link: '#'
-											},
-											{
-												name: 'Performance Crusher',
-												link: '#'
-											},
-											{
-												name: 'Futuristic Experience',
-												link: '#'
-											}
-										]
-									}
-								]
-							},
-							{
-								name: 'Televisions',
-								icon: 'fa fa-desktop',
-								link: '#',
-								items: [
-									{
-										title: 'Televisions',
-										icon: 'fa fa-desktop',
-										link: '#',
-										items: [
-											{
-												name: 'Flat Super Screen',
-												link: '#'
-											},
-											{
-												name: 'Gigantic LED',
-												link: '#'
-											},
-											{
-												name: 'Power Eater',
-												link: '#'
-											},
-											{
-												name: '3D Experience',
-												link: '#'
-											},
-											{
-												name: 'Classic Comfort',
-												link: '#'
-											}
-										]
-									}
-								]
-							},
-							{
-								name: 'Cameras',
-								icon: 'fa fa-camera-retro',
-								link: '#',
-								items: [
-									{
-										title: 'Cameras',
-										icon: 'fa fa-camera-retro',
-										link: '#',
-										items: [
-											{
-												name: 'Smart Shot',
-												link: '#'
-											},
-											{
-												name: 'Power Shooter',
-												link: '#'
-											},
-											{
-												name: 'Easy Photo Maker',
-												link: '#'
-											},
-											{
-												name: 'Super Pixel',
-												link: '#'
-											}
-										]
-									}
-								]
-							}
-						]
-					}
-				]
-			},
-			{
-				name: 'Magazines',
-				icon: 'fa fa-book',
-				link: '#',
-				items: [
-					{
-						title: 'Magazines',
-						icon: 'fa fa-book',
-						items: [
-							{
-								name: 'National Geographics',
-								link: '#'
-							},
-							{
-								name: 'Scientific American',
-								link: '#'
-							},
-							{
-								name: 'The Spectator',
-								link: '#'
-							},
-							{
-								name: 'Rambler',
-								link: '#'
-							},
-							{
-								name: 'Physics World',
-								link: '#'
-							},
-							{
-								name: 'The New Scientist',
-								link: '#'
-							}
-						]
-					}
-				]
-			},
-			{
-				name: 'Store',
-				icon: 'fa fa-shopping-cart',
-				link: '#',
-				items: [
-					{
-						title: 'Store',
-						icon: 'fa fa-shopping-cart',
-						items: [
-							{
-								name: 'Clothes',
-								icon: 'fa fa-tags',
-								link: '#',
-								items: [
-									{
-										title: 'Clothes',
-										icon: 'fa fa-tags',
-										items: [
-											{
-												name: 'Women\'s Clothing',
-												icon: 'fa fa-female',
-												link: '#',
-												items: [
-													{
-														title: 'Women\'s Clothing',
-														icon: 'fa fa-female',
-														items: [
-															{
-																name: 'Tops',
-																link: '#'
-															},
-															{
-																name: 'Dresses',
-																link: '#'
-															},
-															{
-																name: 'Trousers',
-																link: '#'
-															},
-															{
-																name: 'Shoes',
-																link: '#'
-															},
-															{
-																name: 'Sale',
-																link: '#'
-															}
-														]
-													}
-												]
-											},
-											{
-												name: 'Men\'s Clothing',
-												icon: 'fa fa-male',
-												link: '#',
-												items: [
-													{
-														title: 'Men\'s Clothing',
-														icon: 'fa fa-male',
-														items: [
-															{
-																name: 'Shirts',
-																link: '#'
-															},
-															{
-																name: 'Trousers',
-																link: '#'
-															},
-															{
-																name: 'Shoes',
-																link: '#'
-															},
-															{
-																name: 'Sale',
-																link: '#'
-															}
-														]
-													}
-												]
-											}
-										]
-									}
-								]
-							},
-							{
-								name: 'Jewelry',
-								link: '#'
-							},
-							{
-								name: 'Music',
-								link: '#'
-							},
-							{
-								name: 'Grocery',
-								link: '#'
-							}
-						]
-					}
-				]
-			},
-			{
-				name: 'Collections',
-				link: '#'
-			},
-			{
-				name: 'Credits',
-				link: '#'
-			}
-		]
-	}
-];
